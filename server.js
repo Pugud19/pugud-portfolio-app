8d8db26db70c3fe661eca088bed75b7ca48e0aca
Pugud19 <noerbayu73@gmail.com>
2022-03-05 19:11:07 +0700

    edit API on server.js

--- server.js
+++ server.js
@@ -25,11 +25,27 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api", function(req, res){
+  var now = new Date();
+  res.json({
+    "unix": now.getTime(),
+    "utc": now.toUTCString(),
+  })
+})
+
 app.get("/api/:date_string", function(req, res){
   let dateString = req.params.date_string;
-  console.log(dateString);
-  res.json({"error" : "invalid Date"});
-})
+  let passedInValue = new Date(dateString);
+
+  if(passedInValue == "Invalid Date"){
+      res.json({"error" : "invalid Date"});
+  }else(
+    res.json({
+      "unix": passedInValue.getTime(),
+      "utc": passedInValue.toUTCString(),
+    })
+  )
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
