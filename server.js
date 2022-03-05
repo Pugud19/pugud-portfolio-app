fc0992cf1cf5e3a9d65b7ec6906bca36e7d71119
Pugud19 <noerbayu73@gmail.com>
2022-03-05 19:34:39 +0700

    edit and fix invalid Date on server.js

--- server.js
+++ server.js
@@ -37,6 +37,16 @@ app.get("/api/:date_string", function(req, res){
   let dateString = req.params.date_string;
   let passedInValue = new Date(dateString);
 
+  console.log(dateString);
+
+  if( parseInt(dateString) > 10000){
+    let unixTime = new Date(parseInt(dateString));
+    res.json({
+      "unix": unixTime.getTime(),
+      "utc": unixTime.toUTCString(),
+    })
+  }
+
   if(passedInValue == "Invalid Date"){
       res.json({"error" : "invalid Date"});
   }else(
